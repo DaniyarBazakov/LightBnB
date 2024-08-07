a3d04ff877a65c22a6d5980cefa4048885d7c71e
Daniyar Bazakov <bazakovd@gmail.com>
2024-08-07 14:10:05 -0400

    modify getAllReservations

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -129,6 +129,7 @@ const getAllReservations = function (guest_id, limit = 10) {
     JOIN properties ON properties.id = reservations.property_id
     JOIN property_reviews ON properties.id = property_reviews.property_id
     WHERE reservations.guest_id = $1
+      AND reservations.end_date < now()::date
     GROUP BY properties.id, reservations.id
     ORDER BY reservations.start_date
     LIMIT $2;
@@ -145,6 +146,7 @@ const getAllReservations = function (guest_id, limit = 10) {
       console.error(err.message);
     });
 };
+
 
 
 /// Properties
